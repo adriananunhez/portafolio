97b77f5139a59e100fafe2cabd8f7fae0c08743a
Adriana nunez <adri.nunhez@gmail.com>
2017-11-08 11:56:03 -0300

    Componente item y about creado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,17 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { PortafolioComponent } from './components/portafolio/portafolio.component';
+import { AboutComponent } from './components/about/about.component';
+import { PortafolioItemComponent } from './components/portafolio-item/portafolio-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    PortafolioComponent
+    PortafolioComponent,
+    AboutComponent,
+    PortafolioItemComponent
   ],
   imports: [
     BrowserModule,
